aa0f2c1ff3fd8374572caa9ea3981d45090c28ce
Priyanshu <mishrapriyanshu555@gmail.com>
2024-02-12 11:37:12 +0530

    .

--- pages/about.jsx
+++ pages/about.jsx
@@ -5,16 +5,10 @@ const AboutPage = () => {
   return (
     <>
       <h2>Nice to meet you!</h2>
-      <div className={styles.cardContainer}>
-      <div>
-        <p className={styles.heading}>Priyanshu Mishra -> </p>
-        <i>Software Developer </i>
-      </div>
-      <div>
-        <a className ={styles.mail}href="mailto:mishrapriyanshu555@gmail.com">mishrapriyanshu555@gmail.com</a>
-      </div>
-      </div>
       
+      <div className={styles.intro}>
+      Hello,I am Priyanshu Mishra,a final-year engineering student at JSS Academy of Technical Education,Bangalore.Currently pursuing a Bachelor's degree in Information Science and Engineering,I maintain a strong academic record with a current CGPA of 9.2. My technical expertise spans a wide range, including proficiency in React.js,Next.js,Node.js,both NoSQL and SQL databases,Docker,Kubernetes,Java,as well as a keen understanding of problem-solving methodologies.Additionally,I have hands-on experience in developing and implementing RESTful APIs.I am passionate about leveraging my skills to contribute effectively in dynamic and challenging environments.
+      </div>
       <p className={styles.edu}>My Education Journey!</p>
       <div className={styles.edu__content}>
         <div className={styles.junior}>
