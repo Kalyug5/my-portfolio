51b720b2e5153cc58ac7c23d54b24fc817da099e
Priyanshu Mishra <mishrapriyanshu555@gmail.com>
2025-12-21 17:31:40 +0530

    adding snowflakes for the christmas event

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,12 +4,23 @@ import Explorer from '../components/Explorer'
 import Bottombar from '../components/Bottombar'
 import Tabsbar from './Tabsbar'
 import styles from '../styles/Layout.module.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
+import Snowfall from 'react-snowfall'
 
 const Layout = ({ children }) => {
   // set scroll to top of main content on url pathname change
-  const router = useRouter()
+  const router = useRouter();
+  const [snowfallColor,setSnowFallColor] = useState("white");
+
+  useEffect(()=>{
+    const setColorData = setInterval(()=>{
+      setSnowFallColor((prev) => prev === "red" ? "white" : "white");
+    },1000)
+
+    return () => clearInterval(setColorData)
+  },[])
+
   useEffect(() => {
     const main = document.getElementById('main-editor')
     main.scrollTop = 0
@@ -23,6 +34,7 @@ const Layout = ({ children }) => {
         <div style={{ width: '100%' }}>
           <Tabsbar />
           <main id="main-editor" className={styles.content}>
+            <Snowfall color={snowfallColor}/>
             {children}
           </main>
         </div>
